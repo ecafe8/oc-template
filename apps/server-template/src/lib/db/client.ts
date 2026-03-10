2220bc7a0f9b5ef24b3cae0ff2004af91f9eac1e
ben <windzb@qq.com>
2026-03-10 15:28:29 +0800

    fix: 更新数据库连接字符串以使用环境配置

--- apps/server-template/src/lib/db/client.ts
+++ apps/server-template/src/lib/db/client.ts
@@ -1,8 +1,9 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import { env } from "../../config/env";
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: env.DATABASE_URL,
   max: 20,
   idleTimeoutMillis: 30_000,
   connectionTimeoutMillis: 5_000,
